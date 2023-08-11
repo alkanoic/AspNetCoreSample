803f01c8a0f336589e5094e96ad6d8ce2eee3766
alkanoic <alkanoic@gmail.com>
2023-08-11 12:28:24 +0900

    screenshot update

--- e2e/tests/test-2.spec.ts
+++ e2e/tests/test-2.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5182/');
-  await page.getByRole('link', { name: 'Home' }).click();
+  await expect(page).toHaveScreenshot('test-2-1.png');
   await page.getByRole('list').getByRole('link', { name: 'Privacy' }).click();
+  await expect(page).toHaveScreenshot('test-2-2.png');
 });
