01a5c755f0a4c7431b7216c5a0d6c1609755ec5f
alkanoic <alkanoic@gmail.com>
2023-08-13 09:50:14 +0900

    add compose always option add e2e test for production

---
+++ e2e/tests/production/link.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+test('test', async ({ page }) => {
+  await page.goto('https://localhost:7079/');
+  await expect(page).toHaveScreenshot('test-2-1.png');
+  await page.getByRole('list').getByRole('link', { name: 'Privacy' }).click();
+  await expect(page).toHaveScreenshot('test-2-2.png');
+
+  const prisma = new PrismaClient();
+  const name_count = await prisma.name.count();
+  expect(name_count).toBe(3);
+  const name_first = await prisma.name.findFirst();
+  expect(name_first?.name).toBe('太郎');
+});
