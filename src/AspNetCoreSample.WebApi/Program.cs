f0f608dc734236fb878e7c6e42cf6e22afa3542e
alkanoic <alkanoic@gmail.com>
2023-10-22 22:07:02 +0900

    NSwagに変更

--- src/AspNetCoreSample.WebApi/Program.cs
+++ src/AspNetCoreSample.WebApi/Program.cs
@@ -5,15 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
+    app.UseOpenApi(); // serve OpenAPI/Swagger documents
+    app.UseSwaggerUi3(); // serve Swagger UI
+    app.UseReDoc(); // serve ReDoc UI
 }
 
 app.UseHttpsRedirection();
