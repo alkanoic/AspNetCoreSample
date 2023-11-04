75ca91634d14ecbaab0025bf239da7ed3984867f
alkanoic <alkanoic@gmail.com>
2023-11-05 08:20:01 +0900

    devbox 削除

--- install-trivy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.deb
-sudo dpkg -i trivy_0.18.3_Linux-64bit.deb
-rm trivy_0.18.3_Linux-64bit.deb
