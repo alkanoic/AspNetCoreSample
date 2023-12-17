7e0dbc91584e38edf145c056fd84321a610ae9c6
alkanoic <alkanoic@gmail.com>
2023-12-17 18:11:13 +0900

    add dotnet8.0のデプロイに変更

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ az group create --name MyResourceGroup --location japaneast
 az appservice plan create --name MyPlan --resource-group MyResourceGroup --sku F1 --is-linux
 
 # Web Appの作成
-az webapp create --name WebSampleApp2023 --resource-group MyResourceGroup --plan MyPlan --runtime "DOTNET|6.0"
+az webapp create --name WebSampleApp2023 --resource-group MyResourceGroup --plan MyPlan --runtime "DOTNETCORE:8.0"
 
 cd src/AspNetCoreSample.Mvc
 
 # アプリケーションのデプロイ
-az webapp up --name WebSampleApp2023 --resource-group MyResourceGroup --os-type Linux --runtime "DOTNET|6.0"
+az webapp up --name WebSampleApp2023 --resource-group MyResourceGroup --os-type Linux --runtime "DOTNETCORE:8.0"
