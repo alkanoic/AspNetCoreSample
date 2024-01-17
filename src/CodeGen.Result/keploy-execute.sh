da5f6e5f9fb974b7035daf8dc68f9e019202b7ce
alkanoic <alkanoic@gmail.com>
2024-01-18 08:25:41 +0900

    wip keploy local command

--- src/CodeGen.Result/keploy-execute.sh
+++ src/CodeGen.Result/keploy-execute.sh
@@ -1,4 +1,11 @@
-# settings keploy
+# setting local machine keploy
+curl --silent --location "https://github.com/keploy/keploy/releases/latest/download/keploy_linux_amd64.tar.gz" | tar xz -C /tmp && \
+    sudo mkdir -p /usr/local/bin && \
+    sudo mv /tmp/keploy /usr/local/bin
+
+keploy record -c "dotnet run"
+
+# settings docker keploy
 docker network create keploy-network
 alias keploy='sudo docker run --pull always --name keploy-v2 -p 16789:16789 --privileged --pid=host -it -v "$(pwd)":/files -v /sys/fs/cgroup:/sys/fs/cgroup -v /sys/kernel/debug:/sys/kernel/debug -v /sys/fs/bpf:/sys/fs/bpf -v /var/run/docker.sock:/var/run/docker.sock --rm ghcr.io/keploy/keploy'
 
