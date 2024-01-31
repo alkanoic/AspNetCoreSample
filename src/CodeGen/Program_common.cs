b222dc25e346e933d7538ee444bb4b095fcb201f
alkanoic <alkanoic@gmail.com>
2024-01-31 22:28:13 +0900

    fix メソッドを整理

--- src/CodeGen/Program_common.cs
+++ src/CodeGen/Program_common.cs
@@ -28,4 +28,49 @@ public partial class Program
             TemplateOutputFile(type, templateArgs, f, Path.Combine(outputDirectory, f.Replace($"{templateDirectory}/", "").Replace("__ClassName__", className)));
         }
     }
+
+    private static T GetCommonTemplateArgs<T>(string classFilePath, string className, string dbContextPath) where T : CommonTemplateArgs, new()
+    {
+        var targetArgs = new ClassAnalysisArgs()
+        {
+            ClassFilePath = classFilePath,
+            ClassName = className
+        };
+        var targetClassInfo = new ClassAnalysis(targetArgs).ReadCode();
+
+        var dbContextArgs = new ClassAnalysisArgs()
+        {
+            ClassFilePath = dbContextPath
+        };
+        var dbContextClassInfo = new ClassAnalysis(dbContextArgs).ReadCode();
+
+        var controllerName = className + "Controller";
+        var commonTemplateArgs = new T()
+        {
+            UsingNamespaces = TemplateControl.UsingNamespace(targetClassInfo.NamespaceName),
+            ControllerName = controllerName,
+            ContextTypeName = dbContextClassInfo.Name,
+            ModelTypeName = targetClassInfo.Name,
+            EntitySetName = dbContextClassInfo.Properties.Single(x => x.GenericInnerTypeName == targetClassInfo.Name).Name,
+            CompareTargetToArguments = TemplateControl.CompareTargetToArguments(targetClassInfo.PrimaryProperties()),
+            PrimaryKeyNameAttributes = TemplateControl.PrimaryKeyNameAttributes(targetClassInfo.PrimaryProperties()),
+            PrimaryKeyNameArguments = TemplateControl.PropertyNameArguments(targetClassInfo.PrimaryProperties(), true),
+            PrimaryKeyNameTargetArguments = TemplateControl.PrimaryKeyNameTargetArguments(targetClassInfo.PrimaryProperties()),
+            PrimaryKeyNameNewObject = TemplateControl.PrimaryKeyNameNewObject(targetClassInfo.PrimaryProperties()),
+            PrimaryKeyArguments = TemplateControl.PrimaryKeyArguments(targetClassInfo.PrimaryProperties())
+        };
+        commonTemplateArgs.ContextFindPrimaryKey = TemplateControl.ContextFindPrimaryKey(commonTemplateArgs.EntitySetName, targetClassInfo.PrimaryProperties(), true);
+        commonTemplateArgs.CreateBindAttributes = TemplateControl.PropertyNameArguments(targetClassInfo.Properties, false);
+        commonTemplateArgs.EntitySetExist = TemplateControl.EntitySetExist(commonTemplateArgs.EntitySetName, targetClassInfo.PrimaryProperties());
+        commonTemplateArgs.ViewTargetModelHeader = TemplateControl.ViewTargetModelHeader(targetClassInfo.Properties);
+        commonTemplateArgs.ViewTargetModelDetail = TemplateControl.ViewTargetModelDetail(targetClassInfo.Properties);
+        commonTemplateArgs.ViewTargetModelCreate = TemplateControl.ViewTargetModelCreate(targetClassInfo.Properties);
+        commonTemplateArgs.ViewTargetModelDetails = TemplateControl.ViewTargetModelDetails(targetClassInfo.Properties);
+        commonTemplateArgs.ViewTargetModelEditPrimaryKey = TemplateControl.ViewTargetModelEditPrimaryKey(targetClassInfo.PrimaryProperties());
+        commonTemplateArgs.ViewTargetModelEditProperties = TemplateControl.ViewTargetModelEditProperties(targetClassInfo.ExcludePrimaryProperties());
+        commonTemplateArgs.ViewLinkIndexPrimaryKey = TemplateControl.ViewLinkPrimaryKey(targetClassInfo.PrimaryProperties(), "item");
+        commonTemplateArgs.ViewLinkPrimaryKey = TemplateControl.ViewLinkPrimaryKey(targetClassInfo.PrimaryProperties(), "Model");
+
+        return commonTemplateArgs;
+    }
 }
--- src/CodeGen/datatables/Program_datatables.cs
+++ src/CodeGen/datatables/Program_datatables.cs
@@ -10,49 +10,12 @@ public partial class Program
     {
         Console.WriteLine($"DataTables target classFilePath:{classFilePath} className:{className} output:{outputDirectory}");
 
-        var targetArgs = new ClassAnalysisArgs()
-        {
-            ClassFilePath = classFilePath,
-            ClassName = className
-        };
-        var targetClassInfo = new ClassAnalysis(targetArgs).ReadCode();
+        var templateArgs = GetCommonTemplateArgs<DataTablesTemplateArgs>(classFilePath, className, dbContextPath);
 
-        var dbContextArgs = new ClassAnalysisArgs()
-        {
-            ClassFilePath = dbContextPath
-        };
-        var dbContextClassInfo = new ClassAnalysis(dbContextArgs).ReadCode();
+        templateArgs.NamespaceName = namespaceName;
+        templateArgs.Authorize = TemplateControl.AuthorizeAttribute(authorize);
 
-        var controllerName = className + "Controller";
-        var templateArgs = new MvcTemplateArgs()
-        {
-            UsingNamespaces = TemplateControl.UsingNamespace(targetClassInfo.NamespaceName),
-            NamespaceName = namespaceName,
-            Authorize = TemplateControl.AuthorizeAttribute(authorize),
-            ControllerName = controllerName,
-            ContextTypeName = dbContextClassInfo.Name,
-            ModelTypeName = targetClassInfo.Name,
-            EntitySetName = dbContextClassInfo.Properties.Single(x => x.GenericInnerTypeName == targetClassInfo.Name).Name
-        };
-        templateArgs.CompareTargetToArguments = TemplateControl.CompareTargetToArguments(targetClassInfo.PrimaryProperties());
-        templateArgs.PrimaryKeyNameAttributes = TemplateControl.PrimaryKeyNameAttributes(targetClassInfo.PrimaryProperties());
-        templateArgs.PrimaryKeyNameArguments = TemplateControl.PropertyNameArguments(targetClassInfo.PrimaryProperties(), true);
-        templateArgs.PrimaryKeyNameTargetArguments = TemplateControl.PrimaryKeyNameTargetArguments(targetClassInfo.PrimaryProperties());
-        templateArgs.PrimaryKeyNameNewObject = TemplateControl.PrimaryKeyNameNewObject(targetClassInfo.PrimaryProperties());
-        templateArgs.PrimaryKeyArguments = TemplateControl.PrimaryKeyArguments(targetClassInfo.PrimaryProperties());
-        templateArgs.ContextFindPrimaryKey = TemplateControl.ContextFindPrimaryKey(templateArgs.EntitySetName, targetClassInfo.PrimaryProperties(), true);
-        templateArgs.CreateBindAttributes = TemplateControl.PropertyNameArguments(targetClassInfo.Properties, false);
-        templateArgs.EntitySetExist = TemplateControl.EntitySetExist(templateArgs.EntitySetName, targetClassInfo.PrimaryProperties());
-        templateArgs.ViewTargetModelHeader = TemplateControl.ViewTargetModelHeader(targetClassInfo.Properties);
-        templateArgs.ViewTargetModelDetail = TemplateControl.ViewTargetModelDetail(targetClassInfo.Properties);
-        templateArgs.ViewTargetModelCreate = TemplateControl.ViewTargetModelCreate(targetClassInfo.Properties);
-        templateArgs.ViewTargetModelDetails = TemplateControl.ViewTargetModelDetails(targetClassInfo.Properties);
-        templateArgs.ViewTargetModelEditPrimaryKey = TemplateControl.ViewTargetModelEditPrimaryKey(targetClassInfo.PrimaryProperties());
-        templateArgs.ViewTargetModelEditProperties = TemplateControl.ViewTargetModelEditProperties(targetClassInfo.ExcludePrimaryProperties());
-        templateArgs.ViewLinkIndexPrimaryKey = TemplateControl.ViewLinkPrimaryKey(targetClassInfo.PrimaryProperties(), "item");
-        templateArgs.ViewLinkPrimaryKey = TemplateControl.ViewLinkPrimaryKey(targetClassInfo.PrimaryProperties(), "Model");
-
-        TargetTemplateDirectory(typeof(MvcTemplateArgs), templateArgs, "Templates/datatables", outputDirectory, className);
+        TargetTemplateDirectory(typeof(DataTablesTemplateArgs), templateArgs, "Templates/datatables", outputDirectory, className);
 
         Console.WriteLine("Success generate:" + outputDirectory);
     }
