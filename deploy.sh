923e87c5af8758481f32a2d0e832ddc4d11883a7
alkanoic <alkanoic@gmail.com>
2024-03-20 23:14:16 +0900

    add webapi deploy

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,11 @@ az appservice plan create --name MyPlan --resource-group MyResourceGroup --sku F
 cd src/AspNetCoreSample.Mvc
 
 # アプリケーションのデプロイ
-az webapp up --name WebSampleApp2024 --resource-group MyResourceGroup --plan MyPlan --os-type Linux --runtime "DOTNETCORE:8.0"
+az webapp up --name WebSampleApp2024 --resource-group MyResourceGroup --plan MyPlan --os-type Linux --runtime "DOTNETCORE:8.0" --settings WEBSITE_RUN_FROM_PACKAGE=1 DOTNET_VERSION=8.0
+
+az appservice plan create --name MyPlanWebApi --resource-group MyResourceGroup --sku F1 --is-linux --location japanwest
+
+cd src/AspNetCoreSample.WebApi
+
+# アプリケーションのデプロイ
+az webapp up --name WebSampleWebApi2024 --resource-group MyResourceGroup --plan MyPlanWebApi --os-type Linux --runtime "DOTNETCORE:8.0" --location japanwest --settings WEBSITE_RUN_FROM_PACKAGE=1 DOTNET_VERSION=8.0
