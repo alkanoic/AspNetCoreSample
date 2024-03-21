147d385b46368d5a745c56b48af2c1ab9adbde38
alkanoic <alkanoic@gmail.com>
2024-03-21 15:37:02 +0900

    miss debug

--- src/AspNetCoreSample.WebApi/Options/CorsOptions.cs
+++ src/AspNetCoreSample.WebApi/Options/CorsOptions.cs
@@ -2,7 +2,7 @@ namespace AspNetCoreSample.WebApi.Options;
 
 public class CorsOptions
 {
-    public const string Position = "CorsOption";
+    public const string Position = "CorsOptions";
 
     public required string MvcUrl { get; set; }
 }
