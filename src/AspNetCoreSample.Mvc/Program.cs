eabfdb5cc3ebd763076ca3237debbe3997046b95
alkanoic <alkanoic@gmail.com>
2024-06-02 20:36:24 +0900

    add WebOptimizer to js and css minify

--- src/AspNetCoreSample.Mvc/Program.cs
+++ src/AspNetCoreSample.Mvc/Program.cs
@@ -68,6 +68,12 @@ builder.Services.AddSingleton(vapidOption);
 builder.Services.Configure<WebApiOption>(builder.Configuration.GetSection(WebApiOption.Position));
 builder.Services.Configure<JavaScriptOptions>(builder.Configuration.GetSection(nameof(JavaScriptOptions)));
 
+builder.Services.AddWebOptimizer(pipeline =>
+{
+    pipeline.MinifyJsFiles("js/**/*.js");
+    pipeline.MinifyCssFiles("css/**/*.css");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -76,6 +82,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseWebOptimizer();
 }
 
 app.MapDefaultEndpoints();
