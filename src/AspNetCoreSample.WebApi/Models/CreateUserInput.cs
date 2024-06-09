576e907a5ed2622ca12278f33232443b26cbbbc1
alkanoic <alkanoic@gmail.com>
2024-06-09 17:36:07 +0900

    fix email address validation

--- src/AspNetCoreSample.WebApi/Models/CreateUserInput.cs
+++ src/AspNetCoreSample.WebApi/Models/CreateUserInput.cs
@@ -16,9 +16,10 @@ public class CreateUserInputValidator : AbstractValidator<CreateUserInput>
     public CreateUserInputValidator()
     {
         RuleFor(x => x.Username).NotEmpty();
-        RuleFor(x => x.FirstName).NotEmpty();
-        RuleFor(x => x.LastName).NotEmpty();
-        RuleFor(x => x.Email).NotEmpty().EmailAddress();
         RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
+        When(x => !string.IsNullOrWhiteSpace(x.Email), () =>
+        {
+            RuleFor(x => x.Email).EmailAddress();
+        });
     }
 }
