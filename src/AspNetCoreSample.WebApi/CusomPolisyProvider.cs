cc94f95f1459008e14818c815520651f0119466f
alkanoic <alkanoic@gmail.com>
2024-06-19 20:56:34 +0900

    add client-role policy

--- src/AspNetCoreSample.WebApi/CusomPolisyProvider.cs
+++ src/AspNetCoreSample.WebApi/CusomPolisyProvider.cs
@@ -1,6 +1,9 @@
 using System.Collections.Concurrent;
+using System.Security.Claims;
+using System.Text.Json;
 
 using AspNetCoreSample.DataModel.Models;
+using AspNetCoreSample.WebApi.Options;
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -14,16 +17,28 @@ public class CustomAuthorizationPolicyProvider : IAuthorizationPolicyProvider
     private readonly IServiceProvider _serviceProvider;
     private readonly ConcurrentDictionary<string, AuthorizationPolicy> _policies;
     private readonly PolicyService _policyService;
+    private PolicyOptions _policyOptions;
+
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
 
     public CustomAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options,
                                             IServiceProvider serviceProvider,
                                             ConcurrentDictionary<string, AuthorizationPolicy> policies,
-                                            PolicyService policyService)
+                                            PolicyService policyService,
+                                            IOptionsMonitor<PolicyOptions> policyOptions)
     {
         _fallbackPolicyProvider = new DefaultAuthorizationPolicyProvider(options);
         _serviceProvider = serviceProvider;
         _policies = policies;
         _policyService = policyService;
+        _policyOptions = policyOptions.CurrentValue;
+        policyOptions.OnChange(settings =>
+        {
+            _policyOptions = settings;
+        });
     }
 
     public async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
@@ -41,7 +56,32 @@ public class CustomAuthorizationPolicyProvider : IAuthorizationPolicyProvider
             if (roles != null)
             {
                 policy = new AuthorizationPolicyBuilder()
-                    .RequireAssertion(handler => handler.User.HasClaim(c => roles.Contains(c.Value)))
+                    .RequireAssertion(handler =>
+                    {
+                        if (string.IsNullOrEmpty(_policyOptions.ClientRoleName))
+                        {
+                            // Realm-Roleを使用する場合
+                            var json = handler.User.FindFirstValue("realm_access");
+                            if (json is null) return false;
+                            var realmRoles = JsonSerializer.Deserialize<ClientRoles>(json, _jsonSerializerOptions);
+                            if (realmRoles is null || realmRoles.Roles is null) return false;
+                            return roles.Intersect(realmRoles.Roles).Any();
+                        }
+                        else
+                        {
+                            // Client-Roleを使用する場合
+                            var json = handler.User.FindFirstValue("resource_access");
+                            if (json is null) return false;
+                            var rolesDictionary = JsonSerializer.Deserialize<Dictionary<string, ClientRoles>>(json, _jsonSerializerOptions);
+                            if (rolesDictionary is null) return false;
+                            if (rolesDictionary.TryGetValue(_policyOptions.ClientRoleName, out var roleValue))
+                            {
+                                if (roleValue.Roles is null) return false;
+                                return roles.Intersect(roleValue.Roles).Any();
+                            }
+                        }
+                        return false;
+                    })
                     .Build();
 
                 _policies.TryAdd(policyName, policy);
@@ -53,4 +93,9 @@ public class CustomAuthorizationPolicyProvider : IAuthorizationPolicyProvider
 
     public Task<AuthorizationPolicy> GetDefaultPolicyAsync() => _fallbackPolicyProvider.GetDefaultPolicyAsync();
     public Task<AuthorizationPolicy?> GetFallbackPolicyAsync() => _fallbackPolicyProvider.GetFallbackPolicyAsync();
+
+    private sealed class ClientRoles
+    {
+        public List<string>? Roles { get; set; }
+    }
 }
--- src/AspNetCoreSample.WebApi/Options/PolicyOptions.cs
+++ src/AspNetCoreSample.WebApi/Options/PolicyOptions.cs
@@ -3,4 +3,5 @@ namespace AspNetCoreSample.WebApi.Options;
 public class PolicyOptions
 {
     public TimeSpan RefreshPolicyTimeSpan { get; set; } = TimeSpan.FromSeconds(0);
+    public string ClientRoleName { get; set; } = "";
 }
