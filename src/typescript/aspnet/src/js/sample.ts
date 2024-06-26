6a0bbde18e239e1fbc7af2d89f4889c01fec1adc
alkanoic <alkanoic@gmail.com>
2024-06-26 15:49:20 +0900

    viteでtypescriptおよびscssをビルドする

---
+++ src/typescript/aspnet/src/js/sample.ts
@@ -0,0 +1,10 @@
+import { TargetA } from "./interfaces/TargetA";
+
+console.log("aaaa");
+console.log("bbbb");
+
+$(document).on("click", $(".target"), function () {
+  const a = new TargetA();
+  a.Value1 = "a";
+  a.Value2 = 123;
+});
---
+++ src/typescript/aspnet/vite.config.mjs
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import fg from "fast-glob";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    outDir: "wwwroot",
+    sourcemap: process.env.NODE_ENV !== "production",
+    target: "es2020",
+    rollupOptions: {
+      input: Object.fromEntries(
+        fg
+          .sync("{js,css}/*.{ts,js,scss}", {
+            ignore: ["**/_*.{ts,js,scss}"],
+            cwd: "./src",
+          })
+          .map((file) => {
+            const { dir, name } = path.parse(file);
+            return [`${dir}/${name}`, path.resolve("src", file)];
+          })
+      ),
+      // 出力CSSファイル名はassetFileNamesで指定する。inputが.scssなら、[ext]には"css"が入る
+      output: {
+        format: "es",
+        entryFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+
+  // postcssも簡単に指定できる
+  //   css: {
+  //     postcss: {
+  //       plugins: [require("tailwindcss"), require("autoprefixer")],
+  //     },
+  //   },
+});
