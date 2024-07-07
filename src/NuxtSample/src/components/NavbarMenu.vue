715e668e3bc11b615b505808104ab6aaa0325bcc
alkanoic <alkanoic@gmail.com>
2024-07-07 12:09:50 +0000

    change submenu表示に変更

--- src/NuxtSample/src/components/NavbarMenu.vue
+++ src/NuxtSample/src/components/NavbarMenu.vue
@@ -27,88 +27,126 @@
         <label for="my-drawer" class="drawer-overlay" />
         <ul class="min-h-full w-60 bg-gray-800 p-2">
           <li>
-            <ul class="menu">
-              <label class="mb-4 text-xl text-white lg:hidden">NuxtSample</label>
-              <li class="menu-item">
-                <NuxtLink to="/table" :class="{ active: route.path == '/table' }" @click="closeDrawer">Table</NuxtLink>
+            <label class="mb-4 text-xl text-white lg:hidden">NuxtSample</label>
+          </li>
+          <li>
+            <ul class="menu rounded-box">
+              <li>
+                <details open>
+                  <summary class="menu-item">Tables</summary>
+                  <ul>
+                    <li class="menu-item">
+                      <NuxtLink to="/table" :class="{ active: route.path == '/table' }" @click="closeDrawer">Table
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/tabledetail" :class="{ active: route.path == '/tabledetail' }"
+                        @click="closeDrawer">
+                        TableDetail
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/tabulator" :class="{ active: route.path == '/tabulator' }" @click="closeDrawer">
+                        Tabulator</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/tabulator-table" :class="{ active: route.path == '/tabulator-table' }"
+                        @click="closeDrawer">
+                        TabulatorTable</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/tantable" :class="{ active: route.path == '/tantable' }" @click="closeDrawer">
+                        TanStackTable</NuxtLink>
+                    </li>
+                  </ul>
+                </details>
               </li>
-              <li class="menu-item">
-                <NuxtLink to="/tabledetail" :class="{ active: route.path == '/tabledetail' }" @click="closeDrawer">
-                  TableDetail
-                </NuxtLink>
+              <li>
+                <details open>
+                  <summary class="menu-item">Counters</summary>
+                  <ul>
+                    <li class="menu-item">
+                      <NuxtLink to="/ref-counter" :class="{ active: route.path == '/ref-counter' }"
+                        @click="closeDrawer">
+                        RefCounter</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/state-counter" :class="{ active: route.path == '/state-counter' }"
+                        @click="closeDrawer">
+                        StateCounter</NuxtLink>
+                    </li>
+                  </ul>
+                </details>
               </li>
-              <li class="menu-item">
-                <NuxtLink to="/tabulator" :class="{ active: route.path == '/tabulator' }" @click="closeDrawer">
-                  Tabulator</NuxtLink>
+              <li>
+                <details open>
+                  <summary class="menu-item">Auth</summary>
+                  <ul>
+                    <li class="menu-item">
+                      <NuxtLink to="/login" :class="{ active: route.path == '/login' }" @click="closeDrawer">Login
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/logined" :class="{ active: route.path == '/logined' }" @click="closeDrawer">Logined
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/login-admin" :class="{ active: route.path == '/login-admin' }"
+                        @click="closeDrawer">
+                        LoginAdminPage</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/keycloak-logined" :class="{ active: route.path == '/keycloak-logined' }"
+                        @click="closeDrawer">
+                        KeycloakLogined</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/keycloak-admin" :class="{ active: route.path == '/keycloak-admin' }"
+                        @click="closeDrawer">
+                        KeycloakAdminPage</NuxtLink>
+                    </li>
+                  </ul>
+                </details>
               </li>
-              <li class="menu-item">
-                <NuxtLink to="/tabulator-table" :class="{ active: route.path == '/tabulator-table' }"
-                  @click="closeDrawer">
-                  TabulatorTable</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/tantable" :class="{ active: route.path == '/tantable' }" @click="closeDrawer">
-                  TanStackTable</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/ref-counter" :class="{ active: route.path == '/ref-counter' }" @click="closeDrawer">
-                  RefCounter</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/state-counter" :class="{ active: route.path == '/state-counter' }" @click="closeDrawer">
-                  StateCounter</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/parent-child" :class="{ active: route.path == '/parent-child' }" @click="closeDrawer">
-                  ParentChild</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/person" :class="{ active: route.path == '/person' }" @click="closeDrawer">Person
-                </NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/qrod" :class="{ active: route.path == '/qrod' }" @click="closeDrawer">QROD
-                </NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/modal" :class="{ active: route.path == '/modal' }" @click="closeDrawer">Modal
-                </NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/accordion" :class="{ active: route.path == '/accordion' }" @click="closeDrawer">
-                  Accordion</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/validate" :class="{ active: route.path == '/validate' }" @click="closeDrawer">Validate
-                </NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/login" :class="{ active: route.path == '/login' }" @click="closeDrawer">Login
-                </NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/logined" :class="{ active: route.path == '/logined' }" @click="closeDrawer">Logined
-                </NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/login-admin" :class="{ active: route.path == '/login-admin' }" @click="closeDrawer">
-                  LoginAdminPage</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/keycloak-logined" :class="{ active: route.path == '/keycloak-logined' }"
-                  @click="closeDrawer">
-                  KeycloakLogined</NuxtLink>
-              </li>
-              <li class="menu-item">
-                <NuxtLink to="/keycloak-admin" :class="{ active: route.path == '/keycloak-admin' }"
-                  @click="closeDrawer">
-                  KeycloakAdminPage</NuxtLink>
+              <li>
+                <details>
+                  <summary class="menu-item">Others</summary>
+                  <ul>
+                    <li class="menu-item">
+                      <NuxtLink to="/parent-child" :class="{ active: route.path == '/parent-child' }"
+                        @click="closeDrawer">
+                        ParentChild</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/person" :class="{ active: route.path == '/person' }" @click="closeDrawer">Person
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/qrod" :class="{ active: route.path == '/qrod' }" @click="closeDrawer">QROD
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/modal" :class="{ active: route.path == '/modal' }" @click="closeDrawer">Modal
+                      </NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/accordion" :class="{ active: route.path == '/accordion' }" @click="closeDrawer">
+                        Accordion</NuxtLink>
+                    </li>
+                    <li class="menu-item">
+                      <NuxtLink to="/validate" :class="{ active: route.path == '/validate' }" @click="closeDrawer">
+                        Validate
+                      </NuxtLink>
+                    </li>
+                  </ul>
+                </details>
               </li>
             </ul>
           </li>
           <li class="px-2">
             <select id="fruit-select" v-model="selectedFruit" class="select select-bordered w-full max-w-xs"
-              style=" transition-duration: 0s;" @change="handleChange">
+              @change="handleChange">
+              <option disabled selected value="0">Who shot first?</option>
               <option v-for="fruit in fruits" :key="fruit" :value="fruit">
                 {{ fruit }}
               </option>
@@ -133,7 +171,7 @@ const route = useRoute();
 const fruitStore = useFruitStore();
 fruitStore.setDefaults();
 const fruits = fruitStore.fruits;
-const selectedFruit = ref();
+const selectedFruit = ref(0);
 function handleChange(this: unknown, event: unknown) {
   console.log("Selected fruit:", event.target.value);
   fruitStore.setSelectedFruit(event.target.value);
@@ -153,5 +191,9 @@ function handleChange(this: unknown, event: unknown) {
 
 .menu-item a:focus {
   @apply text-white;
+}
+
+.menu :where(li ul):before {
+  @apply bg-white
 }
 </style>
