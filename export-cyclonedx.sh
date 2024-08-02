61e89cd970922411f3b938fefe721e93a0d6279f
alkanoic <alkanoic@gmail.com>
2024-08-03 01:00:25 +0900

    add export sbom and trivy vuls

---
+++ export-cyclonedx.sh
@@ -0,0 +1,9 @@
+dotnet CycloneDX AspNetCoreSample.sln --json --filename dotnet.json --output sbom
+trivy sbom sbom/dotnet.json --format json --output sbom/dotnet-result.json
+
+cyclonedx-npm --output-format json --output-file sbom/AspNetCoreSample.Mvc_npm.json src/AspNetCoreSample.Mvc/package.json
+trivy sbom sbom/AspNetCoreSample.Mvc_npm.json --format json --output sbom/AspNetCoreSample.Mvc_npm-result.json
+
+# javaは個別でビルドすると作成
+# mvn package
+trivy sbom sbom/SpringBoot.Report.json --format json --output sbom/SpringBoot.Report-result.json
