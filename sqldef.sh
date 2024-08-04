c54e0791f4c5b36a2f4c8cb2e45db937935a5923
alkanoic <alkanoic@gmail.com>
2024-08-04 18:05:05 +0900

    sqldefのコマンドを修正

--- sqldef.sh
+++ sqldef.sh
@@ -1,13 +1,33 @@
 # MySQL
 export MYSQL_PWD=mysql
+MYSQL_HOST=mysql
+MYSQL_DB=sample
+MYSQL_USER=root
+MYSQL_PORT=3306
+
 # 現在のスキーマ情報を出力
-mysqldef -u root -h mysql -P 3306 --export sample
-# 現在のスキーマに変更した場合のSQL
-cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | \
-    mysqldef -u root -h mysql -P 3306 --dry-run sample
+mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --export ${MYSQL_DB}
+# 現在のスキーマに変更した場合のSQL syntax errorが出るため以下のスクリプトに変換
+# cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | \
+#   mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --dry-run ${MYSQL_DB}
+cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | sed -E "s/COMMENT\s*'[^']*'//Ig" | \
+    mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --dry-run ${MYSQL_DB} | \
+    awk '/DROP FOREIGN KEY/ {matched = matched $0 "\n";next}{others = others $0 "\n"}END{printf "%s%s", matched, others}' | \
+    sed '/COMMENT = ;/d' | sed '/-- dry run --/d'
+
+# 実際のDBに反映
+# cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | \
+#   mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} ${MYSQL_DB}
+
+# dry-runしたものをmysqlコマンドで実行する方法
+cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | sed -E "s/COMMENT\s*'[^']*'//Ig" | \
+    mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --dry-run ${MYSQL_DB} | \
+    awk '/DROP FOREIGN KEY/ {matched = matched $0 "\n";next}{others = others $0 "\n"}END{printf "%s%s", matched, others}' | \
+    sed '/COMMENT = ;/d' | sed '/-- dry run --/d' | \
+    mysql -u ${MYSQL_USER} -p${MYSQL_PWD} -h ${MYSQL_HOST} ${MYSQL_DB}
 
 # PostgreSQL
-export PGPASSWORD=postgres
+export PGPASSWORD=postgres\
 # 現在のスキーマ情報を出力
 psqldef --user root --host postgresql --port 5432 --export sample
 # 現在のスキーマに変更した場合のSQL
