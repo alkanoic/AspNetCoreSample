f84d08aefaa0a7aec3bbccc068be76ed368128de
alkanoic <alkanoic@gmail.com>
2024-08-05 21:54:12 +0900

    add lit-component sbom

--- export-cyclonedx.sh
+++ export-cyclonedx.sh
@@ -4,6 +4,9 @@ trivy sbom sbom/dotnet.json --format json --output sbom/dotnet-result.json
 cyclonedx-npm --output-format json --output-file sbom/AspNetCoreSample.Mvc_npm.json src/AspNetCoreSample.Mvc/package.json
 trivy sbom sbom/AspNetCoreSample.Mvc_npm.json --format json --output sbom/AspNetCoreSample.Mvc_npm-result.json
 
+cyclonedx-npm --output-format json --output-file sbom/typescript_lit-component_npm.json src/typescript/lit-component/package.json
+trivy sbom sbom/typescript_lit-component_npm.json --format json --output sbom/typescript_lit-component_npm-result.json
+
 # javaは個別でビルドすると作成
 # mvn package
 trivy sbom sbom/SpringBoot.Report.json --format json --output sbom/SpringBoot.Report-result.json
