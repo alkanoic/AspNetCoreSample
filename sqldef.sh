3ce18fcf78cebc0ce7a505e603b18b72d9d31b25
alkanoic <alkanoic@gmail.com>
2024-11-30 21:53:43 +0900

    update sqldef version up

--- sqldef.sh
+++ sqldef.sh
@@ -10,10 +10,10 @@ mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --export ${MYSQL_DB}
 # 現在のスキーマに変更した場合のSQL syntax errorが出るため以下のスクリプトに変換
 # cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | \
 #   mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --dry-run ${MYSQL_DB}
-cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | sed -E "s/COMMENT\s*'[^']*'//Ig" | \
-    mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --dry-run ${MYSQL_DB} | \
-    awk '/DROP FOREIGN KEY/ {matched = matched $0 "\n";next}{others = others $0 "\n"}END{printf "%s%s", matched, others}' | \
-    sed '/COMMENT = ;/d' | sed '/-- dry run --/d'
+
+# A5M2で作成されたDDLのテーブルコメントの) COMMENT '' の部分を COMMENT = ''に変換する
+cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | sed -E "s/^\) COMMENT/\) COMMENT =/Ig" | \
+    mysqldef -u ${MYSQL_USER} -h ${MYSQL_HOST} -P ${MYSQL_PORT} --dry-run ${MYSQL_DB}
 
 # 実際のDBに反映
 # cat .devcontainer/docker/volumes/mysql/initdb.d/0*.sql | \
