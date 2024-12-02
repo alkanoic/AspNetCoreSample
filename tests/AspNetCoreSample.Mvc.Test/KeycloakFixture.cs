95f8a670274faa7e917ce3bbee50910ecf4ebbd6
alkanoic <alkanoic@gmail.com>
2024-12-02 22:46:00 +0900

    update keycloakと連携したMVCのテスト

---
+++ tests/AspNetCoreSample.Mvc.Test/KeycloakFixture.cs
@@ -0,0 +1,45 @@
+using DotNet.Testcontainers.Builders;
+
+using Testcontainers.Keycloak;
+
+using IContainer = DotNet.Testcontainers.Containers.IContainer;
+
+namespace AspNetCoreSample.WebApi.Test;
+
+public sealed class KeycloakFixture : IAsyncLifetime
+{
+    private readonly IContainer _keycloakContainer;
+    private const int KeycloakPort = 8080;
+    private const int KeycloakHealthPort = 9000;
+
+    public KeycloakFixture()
+    {
+        _keycloakContainer = new ContainerBuilder()
+            .WithImage("quay.io/keycloak/keycloak:latest")
+            .WithResourceMapping("Test-realm.json", "/opt/keycloak/data/import/")
+            .WithEnvironment("TZ", "Asia/Tokyo")
+            .WithEnvironment("LANG", "ja_JP.UTF-8")
+            .WithEnvironment("KC_HEALTH_ENABLED", "true")
+            .WithEnvironment("KEYCLOAK_ADMIN", "admin")
+            .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", "passwd")
+            .WithPortBinding(KeycloakPort, true)
+            .WithPortBinding(KeycloakHealthPort, true)
+            .WithCommand("start-dev")
+            .WithCommand("--import-realm")
+            .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(request =>
+                request.ForPath("/health/ready").ForPort(KeycloakHealthPort)))
+            .Build();
+    }
+
+    public string BaseAddress => new UriBuilder(Uri.UriSchemeHttp, _keycloakContainer.Hostname, _keycloakContainer.GetMappedPublicPort(KeycloakBuilder.KeycloakPort)).ToString();
+
+    public Task InitializeAsync()
+    {
+        return _keycloakContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
--- tests/AspNetCoreSample.Mvc.Test/MvcInProcessTest.cs
+++ tests/AspNetCoreSample.Mvc.Test/MvcInProcessTest.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using System.Net;
 using System.Text.Json;
 
+using AspNetCoreSample.WebApi.Test;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,14 +11,16 @@ using Microsoft.Playwright;
 
 namespace AspNetCoreSample.Mvc.Test;
 
-public sealed class MvcInProcessTest : IClassFixture<DbFixture>, IClassFixture<WebApplicationFactoryFixture<Program>>
+public sealed class MvcInProcessTest : IClassFixture<DbFixture>, IClassFixture<KeycloakFixture>, IClassFixture<WebApplicationFactoryFixture<Program>>
 {
     private readonly WebApplicationFactoryFixture<Program> _factory;
 
-    public MvcInProcessTest(DbFixture db, WebApplicationFactoryFixture<Program> factory)
+    public MvcInProcessTest(DbFixture db, KeycloakFixture keycloak, WebApplicationFactoryFixture<Program> factory)
     {
         _factory = factory;
         Environment.SetEnvironmentVariable("ConnectionStrings__Default", db.DbConnectionString);
+        Environment.SetEnvironmentVariable("KeycloakOptions__Authority", $"{keycloak.BaseAddress}auth/realms/Test");
+        Environment.SetEnvironmentVariable("KeycloakOptions__MetadataAddress", $"{keycloak.BaseAddress}realms/Test/.well-known/openid-configuration");
         factory.CreateDefaultClient();
     }
 
@@ -29,6 +33,14 @@ public sealed class MvcInProcessTest : IClassFixture<DbFixture>, IClassFixture<W
         var page = await browser.NewPageAsync();
         await page.GotoAsync($"{_factory.HostUrl}");
 
+        await page.GetByRole(AriaRole.Link, new() { Name = "Auth" }).ClickAsync();
+        await page.GetByLabel("Username or email").FillAsync("admin");
+        await page.GetByLabel("Password", new() { Exact = true }).FillAsync("admin");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Sign In" }).ClickAsync();
+
+        Assert.Contains("Auth Page", await page.TitleAsync());
+
+        await page.GetByRole(AriaRole.Link, new() { Name = "Sample" }).ClickAsync();
         Assert.Contains("AspNetCoreSample.Mvc", await page.TitleAsync());
     }
 }
