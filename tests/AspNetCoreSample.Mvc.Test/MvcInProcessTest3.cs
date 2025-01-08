34ef5edeb25387ce3c450164960d22f79da04534
alkanoic <alkanoic@gmail.com>
2025-01-08 22:10:40 +0900

    change e2e tests

--- tests/AspNetCoreSample.Mvc.Test/MvcInProcessTest3.cs
+++ tests/AspNetCoreSample.Mvc.Test/MvcInProcessTest3.cs
@@ -27,7 +27,12 @@ public sealed class MvcInProcessTest3 : IClassFixture<DbFixture>, IClassFixture<
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(PlaywrightSettings.DefaultBrowserTypeLaunchOptions());
         var page = await browser.NewPageAsync();
-        await page.GotoAsync($"{_factory.HostUrl}");
+        await page.GotoAsync($"{_factory.HostUrl}/Name");
+        await page.GetByRole(AriaRole.Link, new() { Name = "Edit" }).First.ClickAsync();
+        await page.GetByLabel("Name1").ClickAsync();
+        await page.GetByLabel("Name1").ClickAsync();
+        await page.GetByLabel("Name1").FillAsync("太郎123");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Save" }).ClickAsync();
 
         Assert.Contains("AspNetCoreSample.Mvc", await page.TitleAsync());
     }
