9efd5035c8e373afd3360f237d1fd0c758676679
alkanoic <alkanoic@gmail.com>
2025-03-12 20:28:12 +0900

    dotnet aspire

--- src/AspNetCoreSample.AppHost/Program.cs
+++ src/AspNetCoreSample.AppHost/Program.cs
@@ -1,7 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiservice = builder.AddProject<Projects.AspNetCoreSample_WebApi>("SampleWebAPI");
+var postgres = builder.AddPostgres("postgres")
+                      .WithPgAdmin(x => x.WithHostPort(8090));//.WithPgWeb(x => x.WithHostPort(8091));
+
+var postgresdb = postgres.AddDatabase("postgresdb");
+
+var apiservice = builder.AddProject<Projects.AspNetCoreSample_WebApi>("SampleWebAPI")
+                        .WithReference(postgresdb);
 builder.AddProject<Projects.AspNetCoreSample_Mvc>("SampleMvc")
-    .WithReference(apiservice);
+    .WithReference(apiservice)
+    .WithReference(postgresdb);
 
 builder.Build().Run();
