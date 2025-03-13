64b21303ea94733e5547610e892214d93866ca1f
alkanoic <alkanoic@gmail.com>
2025-03-13 22:42:26 +0900

    entity framework mysql to postgres

--- src/AspNetCoreSample.DataModel/Models/Name.cs
+++ src/AspNetCoreSample.DataModel/Models/Name.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 
 namespace AspNetCoreSample.DataModel.Models;
 
+/// <summary>
+/// name
+/// </summary>
 public partial class Name
 {
+    /// <summary>
+    /// id
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// 名前列
+    /// </summary>
     public string Name1 { get; set; } = null!;
 }
--- src/AspNetCoreSample.DataModel/Models/ParentTable.cs
+++ src/AspNetCoreSample.DataModel/Models/ParentTable.cs
@@ -3,26 +3,59 @@ using System.Collections.Generic;
 
 namespace AspNetCoreSample.DataModel.Models;
 
+/// <summary>
+/// parent_table
+/// </summary>
 public partial class ParentTable
 {
+    /// <summary>
+    /// 親テーブルid
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// 親テーブル要素名
+    /// </summary>
     public string TargetName { get; set; } = null!;
 
+    /// <summary>
+    /// 親テーブルint型
+    /// </summary>
     public int? TargetInt { get; set; }
 
+    /// <summary>
+    /// 親テーブルdecimal型
+    /// </summary>
     public decimal? TargetDecimal { get; set; }
 
+    /// <summary>
+    /// 親テーブルdate型
+    /// </summary>
     public DateOnly? TargetDate { get; set; }
 
-    public ulong? TargetBit { get; set; }
+    /// <summary>
+    /// 親テーブルbit型
+    /// </summary>
+    public bool? TargetBit { get; set; }
 
+    /// <summary>
+    /// 作成日時
+    /// </summary>
     public DateTime CreateAt { get; set; }
 
+    /// <summary>
+    /// 作成ユーザー
+    /// </summary>
     public string CreateUser { get; set; } = null!;
 
+    /// <summary>
+    /// 更新日時
+    /// </summary>
     public DateTime UpdateAt { get; set; }
 
+    /// <summary>
+    /// 更新ユーザー
+    /// </summary>
     public string UpdateUser { get; set; } = null!;
 
     public virtual ICollection<ChildTable> ChildTables { get; set; } = new List<ChildTable>();
