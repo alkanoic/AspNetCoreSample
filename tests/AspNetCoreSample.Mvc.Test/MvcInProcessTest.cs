2a756ffc85615e63b39b267654d922e87374fd17
alkanoic <alkanoic@gmail.com>
2025-03-20 09:04:23 +0900

    refactor: simplify MvcInProcessTest classes by removing unused DbFixture and KeycloakFixture dependencies

--- tests/AspNetCoreSample.Mvc.Test/MvcInProcessTest.cs
+++ tests/AspNetCoreSample.Mvc.Test/MvcInProcessTest.cs
@@ -1,26 +1,14 @@
-using System.Diagnostics;
-using System.Net;
-using System.Text.Json;
-
-using AspNetCoreSample.WebApi.Test;
-
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Playwright;
 
 namespace AspNetCoreSample.Mvc.Test;
 
-public sealed class MvcInProcessTest : IClassFixture<DbFixture>, IClassFixture<KeycloakFixture>, IClassFixture<WebApplicationFactoryFixture<Program>>
+public sealed class MvcInProcessTest : IClassFixture<WebApplicationFactoryFixture<Program>>
 {
     private readonly WebApplicationFactoryFixture<Program> _factory;
 
-    public MvcInProcessTest(DbFixture db, KeycloakFixture keycloak, WebApplicationFactoryFixture<Program> factory)
+    public MvcInProcessTest(WebApplicationFactoryFixture<Program> factory)
     {
         _factory = factory;
-        Environment.SetEnvironmentVariable("ConnectionStrings__Default", db.DbConnectionString);
-        Environment.SetEnvironmentVariable("KeycloakOptions__Authority", $"{keycloak.BaseAddress}auth/realms/Test");
-        Environment.SetEnvironmentVariable("KeycloakOptions__MetadataAddress", $"{keycloak.BaseAddress}realms/Test/.well-known/openid-configuration");
         factory.CreateDefaultClient();
     }
 
