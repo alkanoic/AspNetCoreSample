0a13f8262df7f031a878bf67aa92dc3b90689725
alkanoic <alkanoic@gmail.com>
2025-03-25 18:20:50 +0900

    fix: make ServiceProvider nullable in ServiceProviderAccessor

--- src/AspNetCoreSample.WebApi/ServiceProviderAccessor.cs
+++ src/AspNetCoreSample.WebApi/ServiceProviderAccessor.cs
@@ -2,7 +2,7 @@ namespace AspNetCoreSample.WebApi;
 
 public static class ServiceProviderAccessor
 {
-    public static IServiceProvider ServiceProvider { get; private set; }
+    public static IServiceProvider? ServiceProvider { get; private set; }
 
     public static void Initialize(IServiceProvider serviceProvider)
     {
