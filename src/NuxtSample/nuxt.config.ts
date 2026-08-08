ca454560cb1ca26cacbb7a05dfdd65c6b2a54618
alkanoic <alkanoic@gmail.com>
2026-08-08 20:16:28 +0900

    chore(nuxt): compatibilityDate を設定し caniuse-lite を更新する
    - nuxt.config.ts に compatibilityDate を追加して起動時の警告を解消する
    - update-browserslist-db で caniuse-lite を更新して Browserslist の警告を解消する

--- src/NuxtSample/nuxt.config.ts
+++ src/NuxtSample/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2026-08-08",
   // Nuxt modules
   modules: ["@pinia/nuxt", "@vee-validate/nuxt", "@nuxt/ui"],
   ssr: false,
